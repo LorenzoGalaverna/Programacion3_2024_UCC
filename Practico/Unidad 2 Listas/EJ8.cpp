bb43d026f3a9698b4ccc20d970f6b8ed89d0fd76
Lorenzo Galaverna <lorenzo.galaverna1@gmail.com>
2024-08-26 10:20:17 -0300

    fast fix

---
+++ Practico/Unidad 2 Listas/EJ8.cpp
@@ -0,0 +1,22 @@
+// Ejercicio N°8: APLICACIÓN DE CIRCULARIDAD
+// Sistema de Monitor Publicitario Circular
+// Desarrolla un programa para gestionar un monitor publicitario que debe mostrar frases de
+// anuncios de manera circular. Para lograr esto, implementarás una solución utilizando una lista
+// circular simplemente enlazada.
+// Requisitos:
+// 1. Frases de Anuncios:
+// ○ Cada frase de anuncio será un texto simple que el monitor debe mostrar por
+// vez.
+// 2. Mostrar Circularmente:
+// ○ Las frases deben mostrarse en un formato circular, es decir, una vez que se ha
+// mostrado la última frase, el monitor debe volver a mostrar la primera y así
+// infinitamente.
+// 3. Implementación:
+// ○ Utiliza una lista circular simplemente enlazada para gestionar el almacenamiento
+// y la visualización de las frases. La lista circular debe permitir el recorrido
+// continuo de los elementos.
+// 4. Operaciones Básicas:
+// ○ Agregar Frases: Capacidad para añadir nuevas frases a la lista circular.
+// ○ Eliminar Frases: Capacidad para eliminar frases específicas de la lista.
+// ○ Mostrar Frases: Implementa una función para mostrar las frases en el monitor
+// de manera continua, recorriendo circularmente la lista e infinitamente.
